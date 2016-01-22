44f57f6efe33f136e46c672a980e8074ff1bc705
nomenasitraka <noums002@gmail.com>
2016-01-22 11:13:13 +0300

    finish demo_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  resources :microposts
+  resources :microposts
+  resources :microposts
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
